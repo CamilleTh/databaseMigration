424d1d076de4bc645146b047ff44855d50dd18e5
camille <camille.thomassin@intech.lu>
2013-05-27 15:42:25 +0200

    V1

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+import com.googlecode.flyway.core.Flyway;
+import com.googlecode.flyway.core.api.MigrationVersion;
+
+import databaseManager.databaseManagerImpl.DatabaseManagerSQL;
+
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("RUN...");
+	}
+
+}
