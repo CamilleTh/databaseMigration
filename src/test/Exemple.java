ba8426c8818a0603d7ccaef46b1e3d84e22f6ce6
camille <camille.thomassin@intech.lu>
2013-05-28 14:37:01 +0200

    Exemple d'utilisation

---
+++ src/test/Exemple.java
@@ -0,0 +1,34 @@
+package test;
+
+import migrate.Migrate;
+
+public class Exemple {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+    	
+    	// Creation de l'objet
+    	Migrate m = new  Migrate();
+    	
+    	// Lien avec la base et connection 
+    	m.setDataSourceSQL("jdbc:mysql://mysql1.alwaysdata.com/40853_intech", "40853_2", "intech");
+    	
+    	// creation du répertoire pour déposer ces fichiers de migration, creation de la table pour la gestion du flipping
+    	m.init();
+    	
+    	// Initialisation de la base de données, creation du répertoire pour déposer ces fichiers de migration, creation de la table pour la gestion du flipping
+    	m.initAndReset();
+
+    	// migration vers la V1
+    	m.migrateTo("1");
+    	
+    	// Creation d'un boolean de flipping : test
+    	m.createFlipBoolean("test");
+    	
+    	// flipping du boolean de la base
+    	m.flipping("test");
+	}
+
+}
